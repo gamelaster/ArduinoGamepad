4cf743033e238e441bc5c710f9260b1f78ad968f
Marek Kraus <gamieedevelop@outlook.sk>
2016-02-27 13:25:01 +0100

    Adding option to use Rx Ry (default)

--- Gamepad.h
+++ Gamepad.h
@@ -25,34 +25,6 @@
 
 #if defined(_USING_HID)
 
-static const uint8_t ReportDescriptor[] PROGMEM = {
-	0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
-	0x09, 0x05,                    // USAGE (Game Pad)
-	0xa1, 0x01,                    // COLLECTION (Application)
-	0xa1, 0x00,                    //   COLLECTION (Physical)
-	0x85, 0x03,                	   //     REPORT_ID (3)
-	0x05, 0x09,                    //     USAGE_PAGE (Button)
-	0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
-	0x29, 0x10,                    //     USAGE_MAXIMUM (Button 16)
-	0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
-	0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
-	0x75, 0x01,                    //     REPORT_SIZE (1)
-	0x95, 0x10,                    //     REPORT_COUNT (16)
-	0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-	0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
-	0x09, 0x30,                    //     USAGE (X)
-	0x09, 0x31,                    //     USAGE (Y)
-	0x09, 0x32,                    //     USAGE (Z)
-	0x09, 0x33,                    //     USAGE (Rx)
-	0x15, 0x81,                    //     LOGICAL_MINIMUM (-127)
-	0x25, 0x7f,                    //     LOGICAL_MAXIMUM (127)
-	0x75, 0x08,                    //     REPORT_SIZE (8)
-	0x95, 0x04,                    //     REPORT_COUNT (4)
-	0x81, 0x02,                    //     INPUT (Data,Var,Abs)
-	0xc0,                          //     END_COLLECTION
-	0xc0                           // END_COLLECTION
-};
-
 typedef struct
 {
 	uint16_t buttons;
@@ -68,7 +40,7 @@ class Gamepad
 private:
 	gamepad_report_struct reportData;
 public:
-	Gamepad();
+	Gamepad(bool useZRx = false);
 	
 	void sendUpdate();
 	
